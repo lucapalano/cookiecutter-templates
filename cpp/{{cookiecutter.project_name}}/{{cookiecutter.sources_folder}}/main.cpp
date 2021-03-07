41059dc7c45d6c7a974a09c00e7c2faea7576ddd
Luca Palano <luca.palano+git@lpzone.it>
2021-03-07 12:16:08 +0100

    Remove unuseful exclamation point

--- cpp/{{cookiecutter.project_name}}/{{cookiecutter.sources_folder}}/main.cpp
+++ cpp/{{cookiecutter.project_name}}/{{cookiecutter.sources_folder}}/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-    cout << "{{ cookiecutter.greeting }}!" << endl;
+    cout << "{{ cookiecutter.greeting }}" << endl;
 
     return 0;
 }
